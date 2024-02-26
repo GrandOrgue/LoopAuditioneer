f69938c29b852ddefdd097ff82f50f87517fa020
larspalo <larspalo@yahoo.se>
2024-02-26 17:06:34 +0100

    Supply custom pkg-config path for crossbuild.

--- scripts/prepare-ubuntu-cross.sh
+++ scripts/prepare-ubuntu-cross.sh
@@ -8,4 +8,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
   libasound2-dev libpulse-dev libwxgtk3.0-gtk3-dev gcc-mingw-w64-x86-64 \
   g++-mingw-w64-x86-64 autoconf autogen automake build-essential \
   libtool python mingw-w64-tools dpkg-dev
-  
+
+export PKG_CONFIG_LIBDIR=/usr/x86_64-w64-mingw32/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_LIBDIR
