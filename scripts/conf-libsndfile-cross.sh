f7aed81ee8b34b6eed7537f7d2c64b61803a094f
larspalo <larspalo@yahoo.se>
2024-02-26 12:40:58 +0100

    Changed windows cross-build to 64 bit. Added workflow for building on push.

--- scripts/conf-libsndfile-cross.sh
+++ scripts/conf-libsndfile-cross.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # simple script to pass configure command with quotes while cross compiling libsndfile
 # first argument should be cmake_source_dir and the second cmake_binary_dir
-$1/submodules/libsndfile/configure --host=i686-w64-mingw32 --prefix=$2 --enable-static --disable-external-libs --disable-full-suite --disable-sqlite --disable-shared LIBS="-Wl,--as-needed -lssp"
+$1/submodules/libsndfile/configure --host=x86_64-w64-mingw32 --prefix=$2 --enable-static --disable-external-libs --disable-full-suite --disable-sqlite --disable-shared LIBS="-Wl,--as-needed -lssp"
 
