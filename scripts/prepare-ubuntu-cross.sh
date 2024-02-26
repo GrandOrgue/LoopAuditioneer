0925bf41fdd55c96976ab2eadbd8e6d7ca879628
larspalo <larspalo@yahoo.se>
2024-02-26 16:40:23 +0100

    Added crossbuilding dependency.

--- scripts/prepare-ubuntu-cross.sh
+++ scripts/prepare-ubuntu-cross.sh
@@ -7,5 +7,5 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
   libasound2-dev libpulse-dev libwxgtk3.0-gtk3-dev gcc-mingw-w64-x86-64 \
   g++-mingw-w64-x86-64 autoconf autogen automake build-essential \
-  libtool python mingw-w64-tools
+  libtool python mingw-w64-tools dpkg-dev
   
