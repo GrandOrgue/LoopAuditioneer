810d8d54386d9f8a900bac3b924f2949c8d74674
larspalo <larspalo@yahoo.se>
2024-02-26 22:20:26 +0100

    Test a local crossbuild of wxWidgets.

--- scripts/prepare-ubuntu-cross.sh
+++ scripts/prepare-ubuntu-cross.sh
@@ -7,7 +7,4 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
   libasound2-dev libpulse-dev gcc-mingw-w64-x86-64 \
   g++-mingw-w64-x86-64 autoconf autogen automake build-essential \
-  libtool python mingw-w64-tools dpkg-dev
-
-export PKG_CONFIG_LIBDIR=/usr/x86_64-w64-mingw32/lib/pkgconfig
-export PKG_CONFIG_PATH=$PKG_CONFIG_LIBDIR
+  libtool python mingw-w64-tools
