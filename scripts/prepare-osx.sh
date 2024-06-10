b464f082424613e11cd767bd40bbd5ca575e092c
larspalo <larspalo@users.noreply.github.com>
2024-06-10 20:18:48 +0200

    Update prepare-osx.sh
    Added some tools that need to be installed and removed some already existing in the runner images.

--- scripts/prepare-osx.sh
+++ scripts/prepare-osx.sh
@@ -1,7 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
-brew install git cmake wxwidgets libsndfile rtaudio libsamplerate pkgconfig autoconf
-
-# I do not yet know whether we need these packages. They are required by libsndfile,
-# but I assume that they are automatically installed, when you install libsndfile
-# brew install autoconf autogen automake flac libogg libtool libvorbis opus mpg123 pkg-config
+set -e
+brew install wxwidgets pkg-config autoconf autogen automake libtool autoconf-archive
