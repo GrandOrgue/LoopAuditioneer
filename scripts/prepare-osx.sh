18a48c6fe6befba730616e06fe5119905fc968d9
vpoguru <vpo.guru@gmail.com>
2024-06-13 20:43:20 +0200

    Revert "Removed pkg-config from prepare-osx.sh."
    This reverts commit 6ab47b2cf220e392dc6227e518070df2ca11c96c.

--- scripts/prepare-osx.sh
+++ scripts/prepare-osx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-brew install autoconf autoconf-archive autogen automake libtool wxwidgets
+brew install autoconf autoconf-archive autogen automake libtool pkg-config wxwidgets
