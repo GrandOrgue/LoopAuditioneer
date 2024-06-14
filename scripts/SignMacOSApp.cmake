befaac2098164587657ffc6625071c2b557baed0
vpoguru <vpo.guru@gmail.com>
2024-06-14 14:23:11 +0200

    Code signed application built successful. Updated build.yml to create downloadable zip archive.

--- scripts/SignMacOSApp.cmake
+++ scripts/SignMacOSApp.cmake
@@ -1,9 +1,11 @@
 # Script for macOS code signing
 
-if(NOT DEFINED APP_DIR)
-  set(APP_DIR "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_IN_ONE")
-endif()
+set(APP_DIR "${CMAKE_BINARY_DIR}/LoopAuditioneer.app")
 
-execute_process(COMMAND codesign --force --sign - "${APP_DIR}/LoopAuditioneer.app")
-message("Checking code signature...")
-execute_process(COMMAND codesign --verify --deep "${APP_DIR}/LoopAuditioneer.app")
+if(EXISTS "${APP_DIR}")
+    execute_process(COMMAND codesign --force --sign - "${APP_DIR}")
+    message("Checking code signature...")
+    execute_process(COMMAND codesign --verify --deep "${APP_DIR}")
+else()
+    message(FATAL_ERROR "Application bundle not found: ${APP_DIR}")
+endif()
