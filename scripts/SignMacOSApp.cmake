e15a4487f998d7fc77567a4268d281db1c16ead6
vpoguru <vpo.guru@gmail.com>
2024-06-14 10:05:29 +0200

    Revert "1st attempt to build the code signed app."
    This reverts commit 8afd924746fc078839cd9d13b245b9b134044ff0.

--- scripts/SignMacOSApp.cmake
+++
@@ -1,9 +0,0 @@
-# Script for macOS code signing
-
-if(NOT DEFINED APP_DIR)
-  set(APP_DIR "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/ALL_IN_ONE")
-endif()
-
-execute_process(COMMAND codesign --force --sign - "${APP_DIR}/LoopAuditioneer.app")
-message("Checking code signature...")
-execute_process(COMMAND codesign --verify --deep "${APP_DIR}/LoopAuditioneer.app")
