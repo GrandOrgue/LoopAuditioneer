acf303ca9c9c02785a609ae940141ddd64340315
larspalo <larspalo@yahoo.se>
2024-07-02 20:11:08 +0200

    Added a Linux build done with wxWidgets 3.2

---
+++ scripts/prepare-debian-ubuntu-wx32.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+sudo add-apt-repository ppa:sjr/wx32
+sudo apt update
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
+  libjack-dev libasound2-dev libpulse-dev libwxgtk3.2-gtk3-dev \
+  autoconf autogen automake build-essential \
+  libtool python autoconf-archive
+
