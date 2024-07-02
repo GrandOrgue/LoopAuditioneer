df9beb638b784959c7911d5db2894ddd121caeee
larspalo <larspalo@yahoo.se>
2024-07-02 20:28:31 +0200

    Corrected wx3.2 package name in the script.

--- scripts/prepare-debian-ubuntu-wx32.sh
+++ scripts/prepare-debian-ubuntu-wx32.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:sjr/wx32
 sudo apt update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
-  libjack-dev libasound2-dev libpulse-dev libwxgtk3.2-gtk3-dev \
+  libjack-dev libasound2-dev libpulse-dev libwxgtk3.2-dev \
   autoconf autogen automake build-essential \
   libtool python autoconf-archive
 
