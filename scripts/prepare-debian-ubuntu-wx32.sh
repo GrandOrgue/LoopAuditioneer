123db6bcac768d57feceb4cd9377f49eb4434ad6
Lars Palo <lars.birger.palo@gmail.com>
2025-05-12 20:09:30 +0200

    Try fixing plain python package missing by linking with python-is-python3.

--- scripts/prepare-debian-ubuntu-wx32.sh
+++ scripts/prepare-debian-ubuntu-wx32.sh
@@ -8,5 +8,5 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cmake g++ pkg-config \
   libjack-dev libasound2-dev libpulse-dev libwxgtk3.2-dev \
   autoconf autogen automake build-essential \
-  libtool python autoconf-archive
+  libtool python-is-python3 autoconf-archive
 
